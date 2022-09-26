c42489a4b436a31c1eebdf7da175c0dbaa504f2b
Merdancik94 <97890006+Merdancik94@users.noreply.github.com>
2022-09-26 13:03:08 +0500

    Update kurulum+xor.IPHONE.sh

--- kurulum+xor.IPHONE.sh
+++ kurulum+xor.IPHONE.sh
@@ -23,7 +23,7 @@ curl_ip(){
 install_openvpn() {
 	echo ">> OpenVPN kurulumu baslatiliyor"
 	echo ">> indirme islemi basliyor"
-	wget https://github.com/Merdancik94/kurulum-iphone/releases/download/kuurlum-iphone/openvpn-install.IPHONE.sh && bash openvpn-install.IPHONE.sh
+	wget https://raw.githubusercontent.com/Merdancik94/kurulum-iphone/main/openvpn-install%20IPHONE.sh && bash openvpn-install.IPHONE.sh
 	apt-get -y install git curl apache2 libapache2-mod-wsgi python-geoip2 python-ipaddr python-humanize python-bottle python-semantic-version geoip-database-extra geoipupdate
 	echo "Apache config ayalari yapiliyor"
 	echo "WSGIScriptAlias /openvpn-monitor /var/www/html/openvpn-monitor/openvpn-monitor.py" >> /etc/apache2/conf-available/openvpn-monitor.conf
