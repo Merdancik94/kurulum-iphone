df969793a695d108714e1ea0fd5337fb5925aa66
Merdancik94 <97890006+Merdancik94@users.noreply.github.com>
2024-07-10 22:47:40 +0500

    Update openvpn-install.IPHONE.sh

--- openvpn-install.IPHONE.sh
+++ openvpn-install.IPHONE.sh
@@ -422,7 +422,7 @@ persist-key
 persist-tun
 remote-cert-tls server
 auth SHA512
-scramble xormask d
+scramble xormask x
 cipher AES-256-CBC
 ignore-unknown-option block-outside-dns
 block-outside-dns
